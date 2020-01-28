91ae0ed3eee5a03afa4109311f31b39f9791dcd8
Jonas Schwammberger <1262464+lord-blueberry@users.noreply.github.com>
2020-01-28 09:55:56 +0100

    adding data folder

---
+++ p9-data/msToFits.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1969189c44021daf3ca16d1758cd29906839f312714725d366e13aa2d6f24786
+size 2684
---
+++ p9-data/msinput.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5b72ea78a627fd2e7e9a4e54915dba7895e431d62120010bc4e62a59ae3947a4
+size 14313
